ffb2eafa344d8aefdf9ffead338025c21d3676fe
Eduardo Parada <eduardo.parada94@gmail.com>
2022-07-06 15:21:40 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio4_avaliacao_lp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4_avaliacao_lp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+            // Eduardo Augusto Nilson Parada RA 6322529.
+
+            for (int op = 1; op <=50 ; op++)
+            {
+                Console.WriteLine(op);
+
+            }
+            Console.ReadKey();
+         
+
+        }
+    }
+}
